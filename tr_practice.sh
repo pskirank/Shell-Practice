3239a5f3b3387d7b80151309aebd6a56ab7f6d59
pskirank <38557150+pskirank@users.noreply.github.com>
2023-04-04 12:41:48 +0530

    Add files via upload

---
+++ tr_practice.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+#Case-1: TR command to replace () to []
+tr '()' '[]'
+
+#Case-2: TR command to remove all lower case letters
+tr -d 'a-z'
+
+#Case-3: TR command to remove multiple spaces with a single space
+tr -s " "
