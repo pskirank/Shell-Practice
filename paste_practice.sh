1ef31ca5fe4edfabb464e6c529f876282a99b1b1
pskirank <38557150+pskirank@users.noreply.github.com>
2023-04-18 22:49:54 +0530

    Add files via upload

---
+++ paste_practice.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+
+#Challenge-3: https://www.hackerrank.com/challenges/paste-3/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+paste -sd '\t'
+
+#Challenge-4: https://www.hackerrank.com/challenges/paste-4/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+paste -d '\t' - - -
+
+#Challenge-1: https://www.hackerrank.com/challenges/paste-1/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+paste -sd ';'
+
+#Challenge-2: https://www.hackerrank.com/challenges/paste-2/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+paste -d ';' - - -
